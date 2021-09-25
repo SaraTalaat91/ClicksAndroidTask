d59563568837a26fe7881b08dbbe7adeb6d3d617
SaraTMS <sara_talaat91@yahoo.com>
2021-09-25 15:24:12 +0200

    Add feature of filtering the news list

--- app/src/main/java/com/saratms/clickstask/ui/newsList/NewsListFragment.kt
+++ app/src/main/java/com/saratms/clickstask/ui/newsList/NewsListFragment.kt
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.progressindicator.BaseProgressIndicator
 import com.saratms.clickstask.R
 import com.saratms.clickstask.core.State
 import com.saratms.clickstask.databinding.NewsListFragmentBinding
@@ -20,8 +20,10 @@ class NewsListFragment : Fragment() {
     lateinit var newsAdapter: NewsListAdapter
     val newsListViewModel: NewsListViewModel by viewModels()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = NewsListFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -38,14 +40,26 @@ class NewsListFragment : Fragment() {
             binding.searchView.isIconified = false
         }
 
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(keyword: String): Boolean {
+                newsListViewModel.filterByKeyword(keyword)
+                return false
+            }
+        })
+
+
         newsAdapter = NewsListAdapter(requireContext(), mutableListOf())
         binding.newsRecycler.adapter = newsAdapter
         binding.newsRecycler.layoutManager = LinearLayoutManager(requireContext())
     }
 
     private fun setupObservers() {
-        newsListViewModel.newsState.observe(viewLifecycleOwner){
-            when(it){
+        newsListViewModel.newsState.observe(viewLifecycleOwner) {
+            when (it) {
                 is State.LoadingState -> {
                     binding.loadingProgressBar.visibility = View.VISIBLE
                     binding.newsRecycler.visibility = View.GONE
@@ -57,7 +71,6 @@ class NewsListFragment : Fragment() {
                     binding.loadingProgressBar.visibility = View.GONE
                     if (it.data.isEmpty()) {
                         binding.newsRecycler.visibility = View.GONE
-                        binding.searchView.visibility = View.GONE
                         binding.emptyTv.text = getString(R.string.empty_view_statement)
                         binding.emptyTv.visibility = View.VISIBLE
                     } else {
